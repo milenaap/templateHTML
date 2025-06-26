1c05381d849f521e0a27eccf2850807e04d9a748
Dorian Gonzalez <doriandarren1@gmail.com>
2025-06-26 18:29:56 +0200

    fix 0_sass

--- _0_sass/js/script.js
+++ _0_sass/js/script.js
@@ -1,3 +1,8 @@
+
+
+/**
+ * Lógica de ocultar el preload
+ */
 window.addEventListener('load', () => {
   const preloader = document.getElementById('preloader');
   const content = document.getElementById('content');
@@ -9,5 +14,5 @@ window.addEventListener('load', () => {
 
     // Eliminar el preloader del DOM después de la transición
     preloader.addEventListener('transitionend', () => preloader.remove());
-  }, 1000); // Cambia 2000 por el tiempo en milisegundos que desees
+  }, 1000); // Cambia 1000 por el tiempo en milisegundos que desees
 });
