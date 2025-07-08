0c64a4407f030db6ee477888feb8f5126adb8a7e
Dorian Gonzalez <doriandarren1@gmail.com>
2025-07-08 21:23:57 +0200

    add middleware

--- template-astro/src/middleware.js
+++ template-astro/src/middleware.js
@@ -3,10 +3,12 @@ import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   
-    const { url } = context;
-    
+    // const { url } = context;
     context.locals.title = 'Nuevo title';
-    console.log("Middleware ejecutado en:", url.pathname); // ✅ para verificar en terminal
+    // console.log("Middleware ejecutado en:", url.pathname); // ✅ para verificar en terminal
+
+
+    //console.log("PASA por middleware");
 
     return next();
 });
