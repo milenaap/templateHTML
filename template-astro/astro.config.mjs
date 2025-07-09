566997253b8f02c90f4ace01ff68ec9b7c96e94a
Milena <darimile@gmail.com>
2025-07-09 20:42:23 +0200

    add

--- template-astro/astro.config.mjs
+++ template-astro/astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ['es', 'en'],
-        defaultLocale: 'es',
-        routing: { prefixDefaultLocale: true }
-    }
+  i18n: {
+      locales: ['es', 'en'],
+      defaultLocale: 'es',
+      routing: { prefixDefaultLocale: true }
+  },
 
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
