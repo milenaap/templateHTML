6814dd70283ee2bcfa0f3d3abeebeece8e599c73
Dorian Gonzalez <doriandarren1@gmail.com>
2025-07-14 22:10:00 +0200

    fix map

--- template-astro/src/components/maps/Map.jsx
+++ template-astro/src/components/maps/Map.jsx
@@ -9,5 +9,5 @@ export default function Map(){
     L.marker([41.3851, 2.1734]).addTo(map).bindPopup("¡Hola!").openPopup();
   }, []);
 
-  return <div id="map" className="h-[450px] w-full rounded-xl shadow-md"></div>;
+  return <div id="map" className="h-[450px] w-full shadow-md"></div>;
 }
