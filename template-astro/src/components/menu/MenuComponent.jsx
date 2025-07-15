0029aa55a7d542c1b0306eab6003d1c17280beae
Milena <darimile@gmail.com>
2025-07-15 22:52:22 +0200

    fix MenuComponent

--- template-astro/src/components/menu/MenuComponent.jsx
+++ template-astro/src/components/menu/MenuComponent.jsx
@@ -1,20 +1,29 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+
 export const MenuComponent = () => {
   return (
-    <Swiper 
-        slidesPerView={1}
-        spaceBetween={20}
-        pagination={{clickable: true}}
-        navigation
+    
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={1}
+      spaceBetween={20}
+      pagination={{ clickable: true }}
+      navigation
+    //   autoplay={{
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    //   }}
+      loop={true}
+      className="h-full"
     >
-        <SwiperSlide><img src="/images/carta1.jpg" alt="carta1" /></SwiperSlide>    
-        <SwiperSlide><img src="/images/carta2.jpg" alt="carta2" /></SwiperSlide>    
+      <SwiperSlide><img src="/images/carta1.jpg" alt="carta1" className="w-full h-full object-cover" /></SwiperSlide>
+      <SwiperSlide><img src="/images/carta2.jpg" alt="carta2" className="w-full h-full object-cover" /></SwiperSlide>
     </Swiper>
-
 
   )
 }
